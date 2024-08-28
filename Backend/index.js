37ca30c15144017ef08e68867f91524ced685995
Ankit kumar <ankitkumardbg9876@gmail.com>
2024-08-29 00:09:19 +0530

    Express Setup

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+// src/index.js
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
