6b6a7af90f562be89ff132f82b8354e3846c825f
Ankit kumar <ankitkumardbg9876@gmail.com>
2024-08-29 02:32:56 +0530

    update

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,7 @@ app.use(express.json());
 // Write your all the routers in this format
 // import userRouter from "../Routers/userRouter"
 // and use this :-  app.use("api/farmer-app/userRouter")
+// also same for another routers
 
 
 app.use(errorMiddleware);
